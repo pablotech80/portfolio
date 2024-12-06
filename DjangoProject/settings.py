460ac0c3b3c563312a754b3832ccf46c4ecdf2ae
Pablo Techera <ptecherasosa@icloud.com>
2024-12-06 08:57:30 +0100

    Configuraciones iniciales para despliegue

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-kt^+e=gav_2m37*0@1x#iiim*9uicx396qo84!)=)j0i=&@%zs"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+
 
 
 # Application definition
