5b619d7a4e47417afea49264196f18faf557c953
Pablo Techera <ptecherasosa@icloud.com>
2024-12-14 13:58:02 +0100

    modofied DEBUG=True

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),  # Ruta principal para la vista "home"
 ]
+# Esto es necesario para servir los archivos de media
+if settings.DEBUG:  # En desarrollo, Django maneja los archivos automáticamente
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:  # En producción, WhiteNoise manejará los archivos estáticos, pero los de media deben ser gestionados externamente
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
